3e55cdb1945e8b98f03dbde15d5492311255685c
James Vongphasouk <james.vongpha@gmail.com>
2025-10-21 17:18:38 -0600

    09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
 
+	if (numOne < 0 || numTwo < 0) {
+		return "ERROR";
+	}
+	if (!Number.isInteger(numOne) || !Number.isInteger(numTwo)) {
+		return "ERROR";
+	}
+	let total = 0;
+	if (numOne >= numTwo) {
+		for (let i = numTwo; i <= numOne; i++) {
+			total += i;
+		}
+	} else {
+		for (let i = numOne; i <= numTwo; i++) {
+			total += i;
+		}
+	}
+
+	return total
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
