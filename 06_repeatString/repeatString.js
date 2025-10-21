75b4533631a8b047c87693bb31b3b39298441a05
James Vongphasouk <james.vongpha@gmail.com>
2025-10-21 16:08:49 -0600

    06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+	if (num < 0) {
+		return "ERROR"
+	} else {
+		let total = "";
+		for (let i = 0; i < num; i++) {
+			total = total + str;
 
+		}
+		return total;
+
+	}
 };
 
 // Do not edit below this line
