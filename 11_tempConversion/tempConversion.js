ac65000f28e444807718635edfd04054b641a816
James Vongphasouk <james.vongpha@gmail.com>
2025-10-21 18:18:13 -0600

    11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+
+// ```
+// convertToCelsius(32) // fahrenheit to celsius, should return 0
+//
+// convertToFahrenheit(0) // celsius to fahrenheit, should return 32
+// ```
+//
+// Because we are human, we want the result temperature to be rounded to one decimal place: i.e., `convertToCelsius(100)` should return `37.8` and not `37.77777777777778`.
+
+const convertToCelsius = function(farenheight) {
+  return Math.round((farenheight - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return Math.round((celsius * (9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
