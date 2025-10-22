e706102fdeebe14cc6922d2841b837539cfbd439
James Vongphasouk <james.vongpha@gmail.com>
2025-10-22 17:06:28 -0600

    15 and 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,24 @@
-const findTheOldest = function() {
+// Given an array of objects representing people with a birth and death year, return the oldest person.
+//
+// Now that you've reached the final exercise, you should be fairly comfortable getting the information you need from test case(s). Take a look at how the array of objects is constructed in this exercise's test cases to help you write your function.
+//
+// ## Hints
+// - You should return the whole person object, but the tests mostly just check to make sure the name is correct.
+// - There are many ways of doing this using built-in array methods like `reduce`, or even chaining multiple! 
+// - One of the tests checks for people with no death-date.. use JavaScript's Date function to get their age as of today.
+const findTheOldest = function(people) {
+	return people.reduce((acc, cur) => {
+		const curAge = cur.yearOfDeath
+			? cur.yearOfDeath - cur.yearOfBirth
+			: new Date().getFullYear() - cur.yearOfBirth;
+
+		const accAge = acc.yearOfDeath
+			? acc.yearOfDeath - acc.yearOfBirth
+			: new Date().getFullYear() - acc.yearOfBirth;
+
+		// Return the person with greater age
+		return curAge > accAge ? cur : acc;
+	})
 
 };
 
